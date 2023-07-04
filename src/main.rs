19357d854c129b1d451a77e0985fb3168329afa6
ihab65 <ihg.bk65@gmail.com>
2023-07-05 00:49:57 +0100

    parsing into &str elemnts from a markdown file test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::fs;
+
+const TODO: &str = "- [ ]";
+const DONE: &str = "- [x]";
+const _TAB:  &str = "\t";
+
+fn parse (contents: String) {
+    let todos: Vec<_> = contents.lines()
+        .filter(|s| s.starts_with(TODO))
+        .collect();
+
+    let dones: Vec<_> = contents.lines()
+        .filter(|s| s.starts_with(DONE))
+        .collect();
+
+    println!("tasks:");
+    for todo in todos {
+        println!("\t{}", todo)
+    }
+    println!("done :");
+    for done in dones {
+        println!("\t{}", done)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let path: &str = "../../Documents/obsidian.main/To-do.md";
+    let tasks: Result<String, std::io::Error> = fs::read_to_string(path);
+
+    parse(tasks.unwrap())
 }
