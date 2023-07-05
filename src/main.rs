235c6553c718f284fcdf8bd20f5d4f7cbea36fa7
ihab65 <ihg.bk65@gmail.com>
2023-07-05 18:10:29 +0100

    parsing markdown into string elements for later use

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,27 @@ use std::fs;
 
 const TODO: &str = "- [ ]";
 const DONE: &str = "- [x]";
-const _TAB:  &str = "\t";
+const TAB:  &str = "\t";
 
 fn parse (contents: String) {
-    let todos: Vec<_> = contents.lines()
+    let todos: Vec<&str> = contents.lines()
         .filter(|s| s.starts_with(TODO))
         .collect();
+    
+    let sub_tasks: Vec<&str> = contents.lines()
+        .filter(|s| s.starts_with(&(TAB.to_owned() + TODO)))
+        .collect();
 
-    let dones: Vec<_> = contents.lines()
+    let dones: Vec<&str> = contents.lines()
         .filter(|s| s.starts_with(DONE))
         .collect();
 
+
     println!("tasks:");
     for todo in todos {
-        println!("\t{}", todo)
+        println!("\t{}", todo)    
     }
+
     println!("done :");
     for done in dones {
         println!("\t{}", done)
